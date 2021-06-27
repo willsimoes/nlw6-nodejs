542ec333e3a6b22fc5d309798ba3d614c7fbcd11
Willian Simoes <willian.smos@gmail.com>
2021-06-27 10:17:09 -0300

    utilizando middleware de autorização na rota

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,14 @@
-
 import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
-
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
-router.post("/users", createUserController.handle);
-
 const createTagController = new CreateTagController();
-router.post("/tags", createTagController.handle);
+
+router.post("/users", createUserController.handle);
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router }
